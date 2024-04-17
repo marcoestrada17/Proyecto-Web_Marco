efff750302a0b38e53e8d5f03f6ad425e9ecb182
Marco Tulio Estrada Tinoco <100248517+marcoestrada17@users.noreply.github.com>
2024-04-17 08:46:37 -0600

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,44 @@
+const handlerBarsContext = {
+    "/index.html": {
+        "title": "Home"
+    },
+    "/products.html": {
+        "title": "Products"
+    },
+    "/about.html": {
+        "title": "About Us"
+    },
+    "/contact.html": {
+        "title": "Contacts Us"
+    }
+}
+
+  const suplementos = [
+    {title:"Proteina Whey", Image: "img/proteina.jpg"},
+    {title:"BCAA", Image: "img/bcaa.jpg"},
+    {title:"Creatina", Image: "img/creatina.jpg"},
+  ];
+
+  const contactanos = [
+    {title:"Hablanos a", Image: "img/telefono-icono.png", contacto: "Teléfono: 3333-1861"},
+    {title:"Escríbenos a", Image: "img/email-icono.png", contacto: "encreacion@gmail.com"},
+    {title:"Whatsapp", Image: "img/whatsapp-icono.png", contacto: "Teléfono: +504 3333-1861"},
+    {title:"facebook", Image: "img/facebook-icono.png", contacto: "Facebook: https://www.facebook.com/groups/533002513537191 "},
+    {title:"Instagram", Image: "img/intagram-icono.png", contacto: "Instagram: https://www.instagram.com/hector.enamorado.3/ "},
+
+  ];
+
+
+  const servicios = [
+    {title:"Entrenamiento Personalizado",  url:"img/Personalizado.jpg", descripcionimg:"Imagen de Entrenamiento"},
+    {title:"Clases de Grupo",  url:"img/Clases.jpg", descripcionimg:"Imagen de Clases"},
+    {title:"Asesoramiento Nutriciona",  url:"img/Asesoramiento.jpg", descripcionimg:"Imagen de Asesoramiento "},
+    {title:"Área de Cardio y Pesas",  url:"img/cardioypesas.jpg", descripcionimg:"Imagen de Cardio y Pesas"},
+   
+  ];
+
+  const productosm = [
+    {Title:"Suplementos Nutricionaleslas",  url:"img/Suplementos.jpeg", descripcion:"Imagen de caja de Suplementos Nutricionales"},
+    {Title:"Ropa Deportiva",  url:"img/ropa.jpeg", descripcion:"Imagen de Ropa Deportiva"},
+    {Title:"Accesorios de Entrenamiento",  url:"img/accesorios.jpg", descripcion:"Imagen de Accesorios de Entrenamiento"},
+  ];
